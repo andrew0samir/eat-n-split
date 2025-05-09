e1164b020dcf86e58768210d76b5b4bb0a5ed451
andrew0samir <andrewsamir317@gmail.com>
2025-05-09 18:30:41 +0300

    added static splitBill form

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
             <FormAddFriend />
             <Button>Add Friend</Button>
         </div>
+        <FormSplitBill/>
     </div>
 }
 
@@ -65,10 +66,33 @@ function FormAddFriend(){
         <label>🧑‍🤝‍🧑Friend name</label>
         <input type="text" />
 
-        <label>🖼️ Image URL</label>
+        <label>🖼️Image URL</label>
         <input type="text" />
 
         <Button>Add</Button>
 
+    </form>
+}
+
+function FormSplitBill(){
+    return <form className="form-split-bill">
+        <h2>Split a bill with X </h2>
+
+        <label>💰Bill value</label>
+        <input type="text" />
+        
+        <label>🧍Your expense</label>
+        <input type="text" />
+        
+        <label>🧑‍🤝‍🧑X's expense</label>
+        <input type="text" disabled/>
+
+        <label>Who is paying the bill </label>
+        <select>
+            <option value='user'>You</option>
+            <option value='friend'>Your friend</option>
+        </select>
+
+        <Button>Split bill</Button>
     </form>
 }
